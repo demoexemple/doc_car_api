1ce5c456650e561ee8f8866efe21d591b9a1c684
rodes nzie <rodesnzie@gmail.com>
2025-07-22 20:59:39 +0100

    remove un use variable

--- tests/japa.ts
+++ tests/japa.ts
@@ -1,13 +1,10 @@
 import { configure, processCLIArgs, run } from '@japa/runner'
-import { pluginAdonisJS } from '@japa/plugin-adonisjs'
-import testUtils from '@adonisjs/core/services/test_utils'
 
 /**
  * Configure tests
  */
 configure({
   files: ['tests/**/*.spec.ts'],
-  plugins: [pluginAdonisJS()],
   forceExit: false,
 })
 
